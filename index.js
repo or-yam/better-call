2d6a7e7723f0ea2be4dc4cc34f83c80a3044794b
Oryam <48219965+or-yam@users.noreply.github.com>
2022-11-12 13:30:05 +0200

    Split css and js files

---
+++ index.js
@@ -0,0 +1,9 @@
+const nameElement = document.getElementById('name');
+const firstLetterElement = document.getElementById('nameStarter');
+
+const nameInput = window.prompt('Who you gonna call?', 'Soul');
+const nameFirstLetter = nameInput.charAt(0).toUpperCase();
+const nameRest = nameInput.slice(1).toLowerCase();
+
+firstLetterElement.textContent = nameFirstLetter;
+nameElement.childNodes[1].textContent = nameRest;
